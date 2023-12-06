7737b088b9abc428957b89f459bed9091ab6488e
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-12-06 15:12:26 +0100

    added logger

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use crate::utils::shared_state::new_shared_state;
 use axum::Router;
+use tower::ServiceBuilder;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tower_http::LatencyUnit;
+use tracing::Level;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 mod controllers;
 mod services;
@@ -8,12 +14,28 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
-    let shared_state = new_shared_state().await;
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let app = Router::new().nest(
-        "/example",
-        controllers::example_controller::get_router(shared_state.clone()),
-    );
+    let shared_state = new_shared_state().await;
+    let app = Router::new()
+        .nest(
+            "/example",
+            controllers::example_controller::get_router(shared_state.clone()),
+        )
+        .layer(
+            ServiceBuilder::new().layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                    .on_request(DefaultOnRequest::new().level(Level::INFO))
+                    .on_response(
+                        DefaultOnResponse::new()
+                            .level(Level::INFO)
+                            .latency_unit(LatencyUnit::Micros),
+                    ),
+            ),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     println!("Listening on port 8000");
