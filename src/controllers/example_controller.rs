f212243e7a7a5b0cce785ff2d2364a52c14f5289
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-12-06 12:08:27 +0100

    fixed example

--- src/controllers/example_controller.rs
+++ src/controllers/example_controller.rs
@@ -1,19 +1,20 @@
-use crate::utils::shared_state::SharedState;
+use crate::utils::shared_state::{AppState, SharedState};
 use crate::{services};
-use axum::extract::Query;
+use axum::extract::{Query, State};
 use axum::http::StatusCode;
 use axum::routing::get;
-use axum::{Extension, Router};
+use axum::{Router};
 use std::collections::HashMap;
+use std::sync::Arc;
 
 pub fn get_router(shared_state: SharedState) -> Router {
     Router::new()
-        .layer(Extension(shared_state))
         .route("/greet", get(greet))
+        .with_state(shared_state)
 }
 
 async fn greet(
-    Extension(state): Extension<SharedState>,
+    State(state): State<SharedState>,
     Query(params): Query<HashMap<String, String>>,
 ) -> (StatusCode, String) {
     let name = params.get("name").cloned();
